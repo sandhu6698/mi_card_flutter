44de8c3058525c2da2e11a1e17f5708c09aa9d9d
Arshbir.Singh <sandhu6698@gmail.com>
2019-11-26 17:44:04 +0530

    List tile and Row UI \

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,98 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          title: Text("Contacts"),
+          backgroundColor: Colors.teal,
+        ),
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            margin: EdgeInsets.only(top: 35),
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 55,
+                    backgroundImage: AssetImage('images/images.png'),
+                  ),
+                  Text(
+                    "Arshbir Singh",
+                    style: TextStyle(
+                        fontSize: 40,
+                        fontFamily: 'Pacifico',
+                        color: Colors.white),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Text(
+                      "FLUTTER DEVELOPER",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Poppins',
+                          color: Colors.white),
+                    ),
+
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 50),
+                    child: Divider(color: Colors.tealAccent,),
+                  )
+                  ,
+                  Card(
+                    //padding: EdgeInsets.all(10),
+                    color: Colors.white,
+                    margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.phone,
+                            color: Colors.teal,
+                            size: 30,
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            "+919876764867",
+                            style: TextStyle(
+                                color: Colors.teal,
+                                fontSize: 22,
+                                fontFamily: 'Pacifico'),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    //padding: EdgeInsets.all(10),
+                    color: Colors.white,
+                    margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+
+
+                      child: ListTile(
+                        leading: Icon(Icons.email,color: Colors.teal,),
+                        title: Text("sandhu6698@gmail.com",style: TextStyle(fontFamily: 'Pacifico',color: Colors.teal,fontSize: 22),),
+                      ),
+
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
